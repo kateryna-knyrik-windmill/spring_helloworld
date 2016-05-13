57286a2331943c7960250db781883908961b23bc
Katerina Knyrik <kknyrik@archer-soft.com>
2016-05-13 18:00:45 +0300

    add missing files

---
+++ src/main/java/com/kknyrik/euro2016/config/WebInitializer.java
@@ -0,0 +1,56 @@
+package com.kknyrik.euro2016.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.accept.ContentNegotiationManager;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+import org.springframework.web.servlet.view.ContentNegotiatingViewResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Katerina.Knyrik on 5/12/16.
+ */
+public class WebInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class<?>[]{RootConfig.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class<?>[]{RootConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+
+    @Bean
+    public ViewResolver contentNegotiatingViewResolver(
+            ContentNegotiationManager manager) {
+
+        List< ViewResolver > resolvers = new ArrayList< ViewResolver >();
+
+        InternalResourceViewResolver r1 = new InternalResourceViewResolver();
+        r1.setPrefix("/WEB-INF/pages/");
+        r1.setSuffix(".jsp");
+        r1.setViewClass(JstlView.class);
+        resolvers.add(r1);
+
+        JsonViewResolver jsonViewResolver = new JsonViewResolver();
+        resolvers.add(jsonViewResolver);
+
+        ContentNegotiatingViewResolver resolver = new ContentNegotiatingViewResolver();
+        resolver.setViewResolvers(resolvers);
+        resolver.setContentNegotiationManager(manager);
+        return resolver;
+
+    }
+}
